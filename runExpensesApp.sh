a42154020f8da2d17809dcecd98db6140188a6bd
Shirel Pinker <shirelpinker@gmail.com>
2024-02-17 18:39:29 +0200

    fixed run script

--- runExpensesApp.sh
+++ runExpensesApp.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo "starting to run server"
 # Change to Server directory and run npm start:prod
 cd /Users/shirel/Documents/code/shirel-expenses-app/Server/
-npm run start:prod
+npm run start:prod &
 
+echo "starting to run client"
 # Change to Client directory and run npm start
 cd /Users/shirel/Documents/code/shirel-expenses-app/Client
 npm run start
+
+echo "all running !!!"
