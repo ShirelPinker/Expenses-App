9746539f02f7e37022ab45f80d1db8551a51f522
Shirel Pinker <shirelpinker@gmail.com>
2024-02-17 18:34:59 +0200

    added run script

---
+++ runExpensesApp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Change to Server directory and run npm start:prod
+cd /Users/shirel/Documents/code/shirel-expenses-app/Server/
+npm run start:prod
+
+# Change to Client directory and run npm start
+cd /Users/shirel/Documents/code/shirel-expenses-app/Client
+npm run start
